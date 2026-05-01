38b229519276fdba46b9572ed72ff97d486166c1
kobir079 <it24030@mbstu.ac.bd>
2026-05-01 21:33:50 +0600

    Update main (2).java

--- IT24030-30 Days plan/main (2).java
+++ IT24030-30 Days plan/main (2).java
@@ -1,8 +1,8 @@
 public class main(2){
     static void main(String[] args){
         Student s1=new Student();
-        s1.name="Md Jahidul Islam";
-        s1.Roll=IT24018;
+        s1.name="Md Kobir Hosen";
+        s1.Roll=IT24030;
         s1.district="Tangail";
         s1.Number="01775087826";
         s1.Show();
